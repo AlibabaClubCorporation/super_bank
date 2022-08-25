3df0b83952c4e2674dca6a54ae82c9b7ce80bd5b
Ilia Ivko <mrloking11@gmail.com>
2022-08-25 22:48:26 +0200

    Docker set up

--- project/bank/bank_controller/tasks.py
+++
@@ -1,9 +0,0 @@
-from config.celery import app
-
-from .services.general_bank_service import checking_credits_status
-
-
-
-@app.task
-def task_checking_credits_status():
-    checking_credits_status()
